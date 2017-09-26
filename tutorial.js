9f0f1bb53faa4228b2df849681607586389ae460
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-09-26 17:09:49 -0500

    I added some really ugly text - this example was dumb

--- tutorial.js
+++ tutorial.js
@@ -168,3 +168,37 @@ function transformations(){
                            .attr("width", function (d) { return d.width; })
                            .style("fill", function(d) { return d.color; });
 }
+
+function text(){
+	var circleData = [{ "cx": 20, "cy": 20, "radius": 20, "color" : "green" },
+ 					  { "cx": 70, "cy": 70, "radius": 20, "color" : "purple" }];
+
+ 	var svgContainer = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
+
+ 	var circles = svgContainer
+ 		.selectAll("circle")
+ 		.data(circleData)
+ 		.enter()
+ 		.append("circle");
+
+ 	var circleAttributes = circles
+                        .attr("cx", function (d) { return d.cx; })
+                        .attr("cy", function (d) { return d.cy; })
+                        .attr("r", function (d) { return d.radius; })
+                        .style("fill", function (d) { return d.color; });
+
+
+    var text = svgContainer
+ 		.selectAll("text")
+ 		.data(circleData)
+ 		.enter()
+ 		.append("text");
+ 		
+    var textLabels = text
+    				.attr("x", function(d) { return d.cx; })
+    				.attr("y", function(d) { return d.cy; })
+    				.text( function(d) { return "(" + d.cx + ", " + d.cy + ")"; })
+    				.attr("font-family", "sans-serif")
+    				.attr("font-size", "20px")
+    				.attr("fill", "red")
+}
