c4baed115fe67b8bd93e36b4a75bb25ed401cc24
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-09-26 17:29:53 -0500

    created an axis using d3

--- tutorial.js
+++ tutorial.js
@@ -187,13 +187,12 @@ function text(){
                         .attr("r", function (d) { return d.radius; })
                         .style("fill", function (d) { return d.color; });
 
-
     var text = svgContainer
  		.selectAll("text")
  		.data(circleData)
  		.enter()
  		.append("text");
- 		
+
     var textLabels = text
     				.attr("x", function(d) { return d.cx; })
     				.attr("y", function(d) { return d.cy; })
@@ -201,4 +200,27 @@ function text(){
     				.attr("font-family", "sans-serif")
     				.attr("font-size", "20px")
     				.attr("fill", "red")
+}
+
+function axes(){
+	var circleData = [{ "cx": 20, "cy": 20, "radius": 20, "color" : "green" },
+ 					  { "cx": 70, "cy": 70, "radius": 20, "color" : "purple" }];
+	var linearScale = d3.scaleLinear().domain([0, 100]).range([0,400]);
+	var xAxis = d3.axisBottom().scale(linearScale);
+
+
+ 	var svgContainer = d3.select("body").append("svg").attr("width", 400).attr("height", 100);
+	var xAxisGroup = svgContainer.append("g").call(xAxis);
+
+ 	var circles = svgContainer
+ 		.selectAll("circle")
+ 		.data(circleData)
+ 		.enter()
+ 		.append("circle");
+
+ 	var circleAttributes = circles
+                        .attr("cx", function (d) { return d.cx; })
+                        .attr("cy", function (d) { return d.cy; })
+                        .attr("r", function (d) { return d.radius; })
+                        .style("fill", function (d) { return d.color; });
 }
