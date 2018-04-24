adb38df2639c2f93f9d7d4307b01bf236dfe5875
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-04-23 20:50:40 -0500

    begin of work on connection to Twitter API

---
+++ bubbleChart/TwitterConnector/TwitterConnector/TwitterConnector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace TwitterConnector
+{
+    class TwitterConnector
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //Step 1: encode consumer key and secret as base64 seperated by colon
+        private string encodeAPIKeys(string consumerKey, string consumerSecret)
+        {
+            //convert strings to bytes
+            var keyBytes = System.Text.Encoding.UTF8.GetBytes(consumerKey);
+            var secretBytes = System.Text.Encoding.UTF8.GetBytes(consumerSecret);
+            return $"{System.Convert.ToBase64String(keyBytes)}:{System.Convert.ToBase64String(secretBytes)}";
+        }
+
+        private string obtainAccessToken(string authToken)
+        {
+            WebRequest request = WebRequest.Create("https://api.twitter.com/oauth2/token");
+            return "";
+        }
+    }
+}
